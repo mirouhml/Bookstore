3585d9db93a16eb161b267b5fb36d97fe714a250
mirouhml <amarhamlaoui@gmail.com>
2022-03-23 14:35:07 +0100

    Setup project and create all components.

---
+++ src/components/Books.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Books = () => <h2>Books</h2>;
+
+export default Books;
---
+++ src/components/Categories.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Categories = () => <h2>Categories</h2>;
+
+export default Categories;
