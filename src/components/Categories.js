06fb5d1dc6ffa476097fa94b442234da93f649d1
mirouhml <amarhamlaoui@gmail.com>
2022-03-24 03:47:47 +0100

    Add basic styling to Categories.

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './Categories.css';
 
 const Categories = () => (
-  <div>
+  <div className="categories">
     <h2>Categories</h2>
     <button type="button">Check status</button>
   </div>
