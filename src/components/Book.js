50c9677abba8e419f399f965bae8cf392330a3e7
mirouhml <amarhamlaoui@gmail.com>
2022-03-24 02:39:42 +0100

    Implement Book component and basic styling for it.

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import './Book.css';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const Book = (props) => (
-  <li>
-    <h4>{props.category}</h4>
-    <h2>{props.title}</h2>
-    <h3>{props.author}</h3>
-    <p>{props.progress}</p>
+  <li className="book">
+    <div className="book-content-container">
+      <div className="book-content">
+        <h4 className="book-category">{props.category}</h4>
+        <h2 className="book-title">{props.title}</h2>
+        <h3 className="book-author">{props.author}</h3>
+      </div>
+      <div>
+        <button type="button">Comments</button>
+        <button type="button">Remove</button>
+        <button type="button">Edit</button>
+      </div>
+    </div>
+    <div className="progress-content-container">
+      <div className="progress-content">
+        <div className="progress-bar">
+          <CircularProgressbar value={props.progress} text={`${props.progress}%`} />
+        </div>
+        <div className="progress-text">
+          <p className="">{props.progress}%</p>
+          <p className="completed">Completed</p>
+        </div>
+      </div>
+    </div>
+    <div className="vertical-line">
+    </div>
+    <div className="current-chapter-container">
+      <p>CURRENT CHAPTER</p>
+      <p>CHAPTER {props.currentChapter}</p>
+      <button type="button" className="progress-button">UPDATE PROGRESS</button>
+    </div>
   </li>
 );
 
