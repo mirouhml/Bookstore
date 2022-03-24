856772640a65a9ef9bbda2e9416ecb88b4361265
mirouhml <amarhamlaoui@gmail.com>
2022-03-24 03:46:57 +0100

    Fix form errors.

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,8 +6,8 @@ const Form = () => (
     <h2>Add a new book</h2>
     <form>
       <input type="text" placeholder="Book title" />
-      <select id="categories-dropdown" name="categories">
-        <option value="action" select disabled>Categories</option>
+      <select id="categories-dropdown" name="categories" defaultValue="Categories">
+        <option value="categories" disabled>Categories</option>
         <option value="action">Action</option>
         <option value="science fiction">Science Fiction</option>
         <option value="fantasy">Fantasy</option>
