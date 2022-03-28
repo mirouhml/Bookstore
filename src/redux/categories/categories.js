8a3a98530cd096b4bd024f783f7c721472b8d93e
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-03-28 17:55:25 +0100

    Implement actions and reducers for categories.

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,18 @@
+const DISPLAY = 'DISPLAY';
+
+const reducer = (categories = [], action) => {
+  switch (action.type) {
+    case DISPLAY:
+      return action.message;
+    default:
+      return categories;
+  }
+};
+
+const displayMessage = (message) => ({
+  type: DISPLAY,
+  message,
+});
+
+export { displayMessage };
+export default reducer;
