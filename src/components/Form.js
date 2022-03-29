c42a0eb148479cdf66c6e01114b9410666f991b9
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-03-29 10:51:53 +0100

    Set data inputs in react local state.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Form.css';
 import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 
 const Form = (props) => {
   const { addBook } = props;
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
   return (
     <div>
       <h2>Add a new book</h2>
       <form>
-        <input type="text" placeholder="Book title" id="book-title" />
-        <input type="text" placeholder="Author" id="book-author" />
+        <input type="text" placeholder="Book title" id="book-title" value={title} onChange={(event) => setTitle(event.target.value)} />
+        <input type="text" placeholder="Author" id="book-author" value={author} onChange={(event) => setAuthor(event.target.value)} />
         <select id="categories-dropdown" name="categories">
           <option value="action">Action</option>
           <option value="science fiction">Science Fiction</option>
@@ -19,19 +21,19 @@ const Form = (props) => {
         <button
           type="button"
           onClick={() => {
-            const title = document.getElementById('book-title');
-            const author = document.getElementById('book-author');
             addBook({
               id: uuidv4(),
-              title: title.value,
-              author: author.value,
+              title,
+              author,
               category: document.getElementById('categories-dropdown').value,
               progress: 0,
               chapters: 40,
               currentChapter: 1,
             });
-            title.value = '';
-            author.value = '';
+            setTitle('');
+            setAuthor('');
+            document.getElementById('book-title').value = title;
+            document.getElementById('book-author').author.value = author;
           }}
         >
           ADD BOOK
