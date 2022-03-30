06ef4690d71b1af867fd1ecee9328ed33ba3467f
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-03-30 08:31:50 +0100

    Remove eslint disable.

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
